bb17b0b6a340fab0e491f9d992a872e5a125a6a9
Daylon Blakely <DaylonBlakely@lithia.com>
2024-05-03 14:23:58 -0700

    POST /dealerships to publish message

--- publisher-api/src/server.ts
+++ publisher-api/src/server.ts
@@ -1,6 +1,8 @@
 import 'dotenv/config';
 import express from 'express';
 import morgan from 'morgan';
+import { DealershipCreatedPublisher } from 'db-innovation-azure-events';
+import { serviceBusClient } from './service-bus-client';
 
 const PORT = 5000;
 
@@ -11,6 +13,24 @@ app.use(morgan('combined'));
 
 app.get('/', (req, res) => {
   res.send('Publisher - Hello World!');
+});
+
+app.post('/dealerships', async (req, res) => {
+  const min = 1;
+  const max = 1000;
+
+  const dealership = {
+    storeName: 'Test Store',
+    storeNumber: Math.floor(Math.random() * (max - min + 1)) + min,
+  };
+
+  const dealershipCreatedPublisher = new DealershipCreatedPublisher(
+    serviceBusClient
+  );
+
+  dealershipCreatedPublisher.publish(dealership);
+
+  res.send('Dealership created');
 });
 
 app.listen(PORT, () => {
