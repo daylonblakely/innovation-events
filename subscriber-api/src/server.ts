c4d9c4d3f71f81fbf20361209ec10441b08ec1c9
Daylon Blakely <DaylonBlakely@lithia.com>
2024-05-07 17:09:19 -0700

    send subscriber events to client

--- subscriber-api/src/server.ts
+++ subscriber-api/src/server.ts
@@ -1,7 +1,8 @@
-import 'dotenv/config';
+import http from 'http';
 import express from 'express';
 import morgan from 'morgan';
 import cors from 'cors';
+import WebSocket from 'ws';
 import {
   DealershipCreatedSubscriber,
   DealershipCreatedEvent,
@@ -32,11 +33,31 @@ const dealershipCreatedSubscriber = new DealershipCreatedSubscriber(
   process.env.AZURE_SERVICE_BUS_SUBSCRIPTION_NAME || ''
 );
 
+// Create a WebSocket server
+const wss = new WebSocket.Server({ noServer: true });
+
 dealershipCreatedSubscriber.subscribe(async (dealership) => {
   // console.log(dealership);
   dealershipsData.push(dealership);
+
+  // Broadcast the new dealership data to all connected WebSocket clients
+  wss.clients.forEach((client) => {
+    if (client.readyState === WebSocket.OPEN) {
+      client.send(JSON.stringify(dealership));
+    }
+  });
 });
 
-app.listen(PORT, () => {
+// Create an HTTP server
+const server = http.createServer(app);
+
+// Upgrade HTTP server to WebSocket server
+server.on('upgrade', (request, socket, head) => {
+  wss.handleUpgrade(request, socket, head, (ws) => {
+    wss.emit('connection', ws, request);
+  });
+});
+
+server.listen(PORT, () => {
   console.log(`Subscriber API running at http://localhost:${PORT}`);
 });
--- subscriber-client/src/App.tsx
+++ subscriber-client/src/App.tsx
@@ -1,8 +1,46 @@
+import { useEffect, useState } from 'react';
+import { DealershipCreatedEvent } from 'db-innovation-azure-events';
+
 function App() {
+  const [dealerships, setDealerships] = useState<
+    DealershipCreatedEvent['data'][]
+  >([]);
+
+  useEffect(() => {
+    const ws = new WebSocket('ws://localhost:5001');
+
+    ws.onopen = () => {
+      console.log('Connected to WebSocket server');
+    };
+
+    ws.onmessage = (message) => {
+      const dealership = JSON.parse(message.data);
+      setDealerships((prevDealerships) => [...prevDealerships, dealership]);
+    };
+
+    ws.onerror = (error) => {
+      console.log('WebSocket error: ', error);
+    };
+
+    ws.onclose = () => {
+      console.log('WebSocket connection closed');
+    };
+
+    // Clean up the WebSocket connection when the component unmounts
+    return () => ws.close();
+  }, []);
+
   return (
     <>
       <div>
-        <h1>Vite + React - db</h1>
+        <h1>Subscriber Client</h1>
+        <ul>
+          {dealerships.map((dealership, index) => (
+            <li key={index}>
+              {dealership.storeName} - {dealership.storeNumber}
+            </li>
+          ))}
+        </ul>
       </div>
     </>
   );
